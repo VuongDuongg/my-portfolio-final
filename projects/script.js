c879d58a09cfd72c5a89b7bdd2e2483aafbbda9f
VuongDuongg <funnyvuong@gmail.com>
2025-11-15 01:50:05 +0700

    Pj
    as

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Duong Quoc Vuong";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
@@ -31,90 +31,90 @@ document.addEventListener('visibilitychange',
 
 
 // fetch projects start
-function getProjects() {
-    return fetch("projects.json")
-        .then(response => response.json())
-        .then(data => {
-            return data
-        });
-}
+// function getProjects() {
+//     return fetch("projects.json")
+//         .then(response => response.json())
+//         .then(data => {
+//             return data
+//         });
+// }
 
 
-function showProjects(projects) {
-    let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "";
-    projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>`
-    });
-    projectsContainer.innerHTML = projectsHTML;
+// function showProjects(projects) {
+//     let projectsContainer = document.querySelector(".work .box-container");
+//     let projectsHTML = "";
+//     projects.forEach(project => {
+//         projectsHTML += `
+//         <div class="grid-item ${project.category}">
+//         <div class="box tilt" style="width: 380px; margin: 1rem">
+//       <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+//       <div class="content">
+//         <div class="tag">
+//         <h3>${project.name}</h3>
+//         </div>
+//         <div class="desc">
+//           <p>${project.desc}</p>
+//           <div class="btns">
+//             <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+//             <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//     </div>`
+//     });
+//     projectsContainer.innerHTML = projectsHTML;
 
-    // vanilla tilt.js
-    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    //     max: 20,
-    // });
-    // // vanilla tilt.js  
+//     // vanilla tilt.js
+//     // VanillaTilt.init(document.querySelectorAll(".tilt"), {
+//     //     max: 20,
+//     // });
+//     // // vanilla tilt.js  
 
-    // /* ===== SCROLL REVEAL ANIMATION ===== */
-    // const srtop = ScrollReveal({
-    //     origin: 'bottom',
-    //     distance: '80px',
-    //     duration: 1000,
-    //     reset: true
-    // });
+//     // /* ===== SCROLL REVEAL ANIMATION ===== */
+//     // const srtop = ScrollReveal({
+//     //     origin: 'bottom',
+//     //     distance: '80px',
+//     //     duration: 1000,
+//     //     reset: true
+//     // });
 
-    // /* SCROLL PROJECTS */
-    // srtop.reveal('.work .box', { interval: 200 });
+//     // /* SCROLL PROJECTS */
+//     // srtop.reveal('.work .box', { interval: 200 });
 
-    // isotope filter products
-    var $grid = $('.box-container').isotope({
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows',
-        masonry: {
-            columnWidth: 200
-        }
-    });
+//     // isotope filter products
+//     var $grid = $('.box-container').isotope({
+//         itemSelector: '.grid-item',
+//         layoutMode: 'fitRows',
+//         masonry: {
+//             columnWidth: 200
+//         }
+//     });
 
-    // filter items on button click
-    $('.button-group').on('click', 'button', function () {
-        $('.button-group').find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
-}
+//     // filter items on button click
+//     $('.button-group').on('click', 'button', function () {
+//         $('.button-group').find('.is-checked').removeClass('is-checked');
+//         $(this).addClass('is-checked');
+//         var filterValue = $(this).attr('data-filter');
+//         $grid.isotope({ filter: filterValue });
+//     });
+// }
 
-getProjects().then(data => {
-    showProjects(data);
-})
-// fetch projects end
+// getProjects().then(data => {
+//     showProjects(data);
+// })
+// // fetch projects end
 
-// Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
+// // Start of Tawk.to Live Chat
+// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+// (function () {
+//     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+//     s1.async = true;
+//     s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+//     s1.charset = 'UTF-8';
+//     s1.setAttribute('crossorigin', '*');
+//     s0.parentNode.insertBefore(s1, s0);
+// })();
 // End of Tawk.to Live Chat
 
 // disable developer mode
